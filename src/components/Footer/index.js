249f0b4f94cf23e7a49bd44dd6dcfb57c69b2ca0
titenq <titenq@gmail.com>
2021-05-01 23:01:28 -0300

    footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,43 @@
+import { FaGithub, FaTelegramPlane, FaTwitter } from 'react-icons/fa';
+import styles from './Footer.module.css';
+
+const links = [
+  {
+    href: 'https://github.com/titenq',
+    icon: <FaGithub className={styles.icon} />
+  },
+  {
+    href: 'https://t.me/titenq',
+    icon: <FaTelegramPlane className={styles.icon} />
+  },
+  {
+    href: 'https://twitter.com/titenq',
+    icon: <FaTwitter className={styles.icon} />
+  },
+];
+
 const Footer = () => {
   return (
-    <h1>
-      Footer
-    </h1>
-  )
+    <footer>
+      <h2 className={styles.titenq}>TitenQ <sup>&copy;</sup></h2>
+      <ul>
+        {links.map((link, index) => {
+          return (
+            <li key={index}>
+              <a 
+                href={link.href}
+                target='_blank'
+                rel='noopener noreferrer'
+                className={styles.icon_button}
+              >
+                {link.icon}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
